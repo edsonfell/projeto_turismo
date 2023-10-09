92e4e55d0f7999c29851f85dfe54c82b57cfa9a5
Edson Fell <edsonfell22@gmail.com>
2023-10-09 12:13:07 -0300

    created main function to download csv and json file with URLs we desired to work with

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import functions_framework
+import requests
+from google.cloud import storage
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@functions_framework.http
+def hello_http(request):
+    print('Iniciando processo de download...')
+    request_json = request.get_json(silent=True)
+    print(f"Parametros recebidos: {request_json}")
+    bucket_name = "turismo_bucket"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("Inicializando o cliente de armazenamento do Google Cloud")
+    storage_client = storage.Client()
+    # Obtém o bucket
+    print("Criando client do bucket")
+    bucket = storage_client.bucket(bucket_name)
+    for csv_url in request_json['csv_to_download']:
+        file_name = csv_url.split("/")[-1]
+        try:
+            print(f"Iniciando download do arquivo")
+            response = requests.get(csv_url)
+            if response.status_code == 200:
+                # Cria um objeto de blob no bucket
+                blob = bucket.blob(file_name)
+
+                # Faz o upload do conteúdo do arquivo para o blob
+                blob.upload_from_string(response.text, content_type='text/csv')
+
+                print(f"Arquivo CSV baixado e salvo em gs://{bucket_name}/{file_name}")
+            else:
+                print("Falha ao baixar o arquivo CSV!")
+        except Exception as e:
+            print(f"Erro: {e}")
